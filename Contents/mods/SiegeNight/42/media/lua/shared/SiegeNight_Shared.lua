490a563f51389650323ebba4a4312b0baf47b7e8
Your Name <you@email.com>
2026-02-23 15:51:45 -0600

    v2.5.4 - fix chat command prefix, update changelog and version
    - Bumped version to 2.5.4
    - Updated mod.info description (was showing v2.4.0, now v2.5.4)
    - Updated mod.info modversion to 2.5.4
    - Added v2.5.2 and v2.5.4 to workshop changelog
    - Updated all changelog /siege references to !siege
    - Added 'Chat commands + vote system' to roadmap current features

--- Contents/mods/SiegeNight/42/media/lua/shared/SiegeNight_Shared.lua
+++ Contents/mods/SiegeNight/42/media/lua/shared/SiegeNight_Shared.lua
@@ -9,7 +9,7 @@ local SN = {}
 -- ==========================================
 -- VERSION
 -- ==========================================
-SN.VERSION = "2.5.3"
+SN.VERSION = "2.5.4"
 SN.MOD_ID = "SiegeNight"
 SN.CLIENT_MODULE = "SiegeNightModule"
 
